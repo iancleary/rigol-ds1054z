9987337805d87121203ddf9edad7e4209a9fc3b3
Ian Cleary <github@iancleary.me>
2025-09-14 15:07:07 -0700

    Justfile, formatting with ruff

---
+++ justfile
@@ -0,0 +1,17 @@
+help:
+  just --list
+
+run:
+  uv run main.py
+
+install:
+  uv install
+
+visa:
+  echo "In order to use this module, you must have the VISA platform installed for your operating system." 
+  echo "For Windows, you can download and install the NI-VISA runtime from:"
+  echo "    https://www.ni.com/en-us/support/downloads/drivers/download.ni-visa.html"
+  echo "For both Linux and MacOS, you can use the same URL as Windows to download the NI-VISA runtime."
+
+format:
+  uv run ruff format .
--- main.py
+++ main.py
@@ -6,10 +6,9 @@ import matplotlib.pyplot as plt
 import time
 
 
-
 def main():
     print("Hello from rigol!")
-    
+
     rm = pyvisa.ResourceManager()
 
     # We are connecting the oscilloscope through USB here.
@@ -23,37 +22,37 @@ def main():
 
     osc = Rigol1000z.Rigol1000z(osc_resource)
     print(osc)
-    
+
     print("Stopping oscilloscope")
     osc.stop()
-    
+
     print("Waiting 1 second")
     time.sleep(1)
-    
+
     print("Running oscilloscope")
     osc.run()
-    
+
     # time.sleep(5)
-    
+
     # osc.autoscale()
 
     # Change voltage range of channel 1 to 50mV/div.
     # osc[1].set_vertical_scale_V(1000e-3)
     print("Waiting 2 seconds")
     time.sleep(2)
-    
+
     channel1 = osc[1]
     print(channel1)
-    
+
     # osc.visa_write(":WAV:DATA? CHAN1")
     # raw_data = osc.visa_read_raw(250000)
     # data = channel1.parse_waveform_data(raw_data)
     # print(raw_data)
-    
+
     info = channel1.get_data_premable()
     print(info)
-    
-    (t, v) = channel1.get_data(mode="norm", filename='channel1.dat')
+
+    (t, v) = channel1.get_data(mode="norm", filename="channel1.dat")
     print("Data from channel 1 written to channel1.dat")
     print(t)
     print(v)
@@ -63,14 +62,14 @@ def main():
 
     # Take a screenshot.
     print("Taking screenshot")
-    osc.get_screenshot('screenshot.png', 'png')
-    
+    osc.get_screenshot("screenshot.png", "png")
+
     print("Stopping oscilloscope")
     osc.stop()
-    
-    trace = pd.DataFrame({'time': t, 'voltage': v})
+
+    trace = pd.DataFrame({"time": t, "voltage": v})
     print(trace)
-    trace.plot(x='time', y='voltage')
+    trace.plot(x="time", y="voltage")
     plt.savefig("channel1.png")
 
     # Capture the data sets from channels 1--4 and=
